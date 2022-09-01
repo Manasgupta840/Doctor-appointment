528da7c96d761443c47afb1547e3ea969307bacc
Manas <guptamanas840@gmail.com>
2022-09-01 14:15:10 +0530

    image file added

--- src/main/java/com/doctor/appointment/controllers/DoctorInfoController.java
+++ src/main/java/com/doctor/appointment/controllers/DoctorInfoController.java
@@ -1,11 +1,17 @@
 package com.doctor.appointment.controllers;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.hibernate.engine.jdbc.StreamUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,12 +21,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.doctor.appointment.config.ApiResponse;
 import com.doctor.appointment.models.DoctorInfo;
-import com.doctor.appointment.models.Hospital;
 import com.doctor.appointment.service.DoctorInfoService;
+import com.doctor.appointment.service.FileService;
+
+
 
 @RestController
 @RequestMapping("/api/doctor-info")
@@ -29,6 +40,12 @@ public class DoctorInfoController {
 	
 	@Autowired
 	private DoctorInfoService doctorInfoservice;
+	
+	@Autowired
+	private FileService fileService;
+
+	@Value("${project.image}")
+	private String path;
 	
 	//Post - create doctorInfo
 		@PostMapping("/doctor/{doctorId}")
@@ -79,6 +96,35 @@ public class DoctorInfoController {
 			return new ResponseEntity<List<DoctorInfo>>(doctorInfo,HttpStatus.OK);
 		}
 		
+		// property image upload
+		@CrossOrigin
+		@RequestMapping(method = RequestMethod.POST, path = "/doctor/image/upload/{id}")
+		public ResponseEntity<DoctorInfo> uploadPostImage(@RequestParam("image") MultipartFile image,
+				@PathVariable Integer id) throws IOException {
+	         DoctorInfo doctorInfo = this.doctorInfoservice.getDoctorInfoById(id);
+			String fileName = this.fileService.UploadImage(path, image);
+					
+			doctorInfo.setImageName(fileName);
+			System.out.println("The file Name is "+fileName);
+
+		DoctorInfo updatedoctorInfo=	this.doctorInfoservice.updateDoctorInfo(doctorInfo, id);
+		return new ResponseEntity<DoctorInfo>(updatedoctorInfo,HttpStatus.OK);
+		}
+		
+		// serve image files 
+		
+		@GetMapping(value="/property/image/{imageName}", produces=MediaType.IMAGE_JPEG_VALUE)
+		public void downloadImage(
+				@PathVariable("imageName") String imageName,
+				HttpServletResponse response
+				
+				) throws IOException {
+			
+			InputStream resource =this.fileService.getResource(path, imageName);
+			response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+			StreamUtils.copy(resource,response.getOutputStream());
+			
+		}
 	
 
 }
---
+++ src/main/java/com/doctor/appointment/service/FileService.java
@@ -0,0 +1,55 @@
+package com.doctor.appointment.service;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+public class FileService {
+
+	public String UploadImage(String path, MultipartFile file) throws IOException {
+		// file name
+		
+		String name=file.getOriginalFilename();
+		
+		// Random name generate file 
+		
+		String randomID= UUID.randomUUID().toString();
+		String fileName1=randomID.concat(name.substring(name.lastIndexOf(".")));
+		
+		// fullpath 
+		
+		String filePath=path+File.separator + fileName1;
+		
+		// create folder if not created 
+		
+		File f= new File(path);
+		if(!f.exists()) {
+			f.mkdir();
+		}
+		
+		// file copy 
+		
+		Files.copy(file.getInputStream(), Paths.get(filePath));
+//		
+//		System.out.println("the name is "+name);
+//		System.out.println("the fileName1 is"+fileName1);
+//		System.out.println("the filepath is "+filePath);
+		return fileName1;
+	}
+
+	public InputStream getResource(String path, String fileName) throws FileNotFoundException {
+		String fullPath=path+File.separator+fileName;
+		InputStream is = new FileInputStream(fullPath);
+		return is;
+	}
+
+}
